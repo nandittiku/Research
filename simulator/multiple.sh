fac019503955da69b4421f252f7a328498b68e2a
Nandit Tiku <nandittiku@gmail.com>
2012-03-16 16:15:35 -0500

    added dht lookup plot

--- simulator/multiple.sh
+++ simulator/multiple.sh
@@ -9,10 +9,13 @@ let mean_alive=3600
 simulation_time=1000
 rLookup=10
 enable_dht_attack=1
-single_succ=1
+single_succ=0
 
 for x in 1 2 3 4 5
 do
-	random_seed=$RANDOM
-	time ./sim $num_nodes $check_predecessor_timer $stabilize_timer $fix_fingers_timer $sign_timer $path_timer $mean_alive $simulation_time $random_seed $rLookup $single_succ $enable_dht_attack > data/dht/many-$x.txt
+	for y in 1 5 10 15 20
+	do
+		random_seed=$RANDOM
+		time ./sim $num_nodes $check_predecessor_timer $stabilize_timer $fix_fingers_timer $sign_timer $path_timer $mean_alive $simulation_time $random_seed $y $single_succ $enable_dht_attack > data/dht/r=$y--$x.txt
+	done
 done
